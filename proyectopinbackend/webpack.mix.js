4ea0a12f2454de6d9aec78d0b50a2598b62e3292
Cesarozamis <cesarozamis@hotmail.com>
2022-03-25 08:02:15 -0300

    work

--- proyectopinbackend/webpack.mix.js
+++ proyectopinbackend/webpack.mix.js
@@ -1,4 +1,3 @@
-const { react } = require('laravel-mix');
 const mix = require('laravel-mix');
 
 /*
@@ -13,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .react()
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
